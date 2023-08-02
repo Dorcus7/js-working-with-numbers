b1d3f1329b4a77686f6696ddc3c92c379361d06a
Laura Hackney <laura@anniecannons.com>
2023-08-02 14:51:03 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Working with Numbers
+
+### Arithmetic Operators
+
+- As a developer, you will have a lot of tools that you can use to calculate values in JavaScript. This can be helpful in building products for stores, games, or other applications.
+- Here are the operators: 
+- Addition (+): Adds two or more numbers together.
+- Subtraction (-): Subtracts one number from another.
+- Multiplication (*): Multiplies two or more numbers.
+- Division (/): Divides one number by another.
+- Modulus (%): Returns the remainder after division.
+
+- JavaScript follows the standard mathematical rules of precedence (PEMDAS), where multiplication and division are evaluated before addition and subtraction. You can use parentheses to control the order of operations if needed.
+
+### Assignment Operator
+
+- Assignment Operator: Remember, the equal sign, =, is the assignment operator and not a traditional equal sign. This is how you can assign and reassign values in JavaScript!
+
+### Math Object
+
+- Math Object: JavaScript comes with the Math object that provides handy tools for running certain calculations. [Here is the MDN article on the Math Object.](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math)
+
+### NaN Value
+
+- NaN stands for "Not-a-Number." It is a special value used to represent the result of an operation that cannot produce a meaningful numeric result. When you perform certain arithmetic operations in JavaScript, the result may be undefined or not a valid number. In such cases, the result is represented as NaN. 
+
+
+
+### Prep
+1. Connect your js file to your HTML file
+2. Open your code in the browser and open your console
+
+
+### Exerise 1 - Reading Code
+1. In the `js` file, carefully review the code listed.
+2. Answer the questions about the code and run it in the browser.
+
+
+### Exerise 2 - Operators
+1. In the `js` file, complete the activities on using different operators.
+
+
+### Exerise 3 - Decades Calculator
+1. In the `js` file, follow the prompts to calculate the amount of snacks you will need to last you for the next decade.
+
+
+### Exerise 4 - PEMDAS
+1. In the `js` file, follow the prompts to calculate average scores of each student's subjects.
+
+
+### Exercise 5 - Weather Converter
+1. In the `js` file, follow the prompts to calculate farenheit based on celsius temperatures.
+
+
+### Exerise 6 - Assigning Values
+- You will regularly need to reassign the values of variables based on what you need to be tracking as a user interacts with your program.
+1. In the `js` file, follow the prompts to reassign the values of a variable.
+
+
+### Exerise 7 - Math Object
+- The Math Object is a powerful tool for helping you with certain math calculations.
+1. In the `js` file, follow the prompts to round a number.
+
+
+### Exerise 8 - Math Object
+1. In the `js` file, follow the prompts to generate a random number for a dice game, and then alert the user of the value.
+
+
+### Exerise 9 - NaN Values
+1. In the `js` file, follow the prompts to fix the code of its NaN values. 
+2. Look up the isNaN() operator in MDN.
---
+++ scripts.js
@@ -0,0 +1,124 @@
+/********************** Exercise 1  **********************/
+// Describe the following code:
+
+const wholesalePrice = 5.45;
+const retailPrice = 9.99;
+const quantity = 47;
+const salesTotal = retailPrice * quantity;
+const profit = salesTotal - (wholesalePrice * quantity);
+
+// Where might this code be used?
+
+
+// Run this code and log the profit variable in the console. What answer do you get?
+
+
+// How would you change this code to make the value of the quanitity variable dynamic?
+
+
+/********************** Exercise 2 - Operators **********************/
+// Step 1: Calculate the sum of two numbers and log the result.
+
+
+// Step 2: Calculate the difference between two numbers and print the result.
+
+
+// Step 3: Calculate the product of two numbers and print the result.
+
+
+// Step 4: Calculate the result of dividing two numbers and print the result. Handle division by zero.
+
+
+// Step 5: Check if a given number is even and print the result.
+
+
+
+/********************** Exercise 3 - Decades Calculator **********************/
+// Store your current age into a variable.
+// Store a maximum age into a variable (10 years older than you are now).
+// Store an estimated amount per day (as a number).
+// Calculate how many you would eat total for the next decade.
+// Output the result to the screen like so: "You will need [AMOUNT] to last you until the age of [AGE]".
+
+
+
+
+
+
+/********************** Exercise 4 - PEMDAS **********************/
+// You are working on an education app and you want to be able to provide the average scores for students.
+// Here are the stored variables for a student and their subjects.
+const mathScore = 90;
+const scienceScore = 85;
+const englishScore = 80;
+const historyScore = 95;
+// How would you get the average of their scores using JavaScript?
+
+
+
+
+
+
+/********************** Exercise 5 - Weather Converter **********************/
+// Write a calculation that converts celsius to fahrenheit. To get the value of fahrenheit, you have to multiply the celsius value by 9/5 and then add 32. 
+
+
+// Prompt the user for the value of the temperature in celcius. 
+
+
+// Run the code so that the following is logged to the console: 25°C is equal to 77°F
+
+
+
+
+/********************** Exercise 6 - Assigning Values  **********************/
+
+// Prompt the user for two numbers.
+
+// Make sure that the values of the variables (the strings) are converted to numbers. 
+
+// Create a new variable, add the numbers, and log the value.
+
+// Reassign the variable to store the values of the numbers being substracted. Log the value.
+
+// Add 100 to the variable. Log the value.
+
+// Divide the value of the variable by 20. Log the value. 
+
+
+
+
+
+
+/********************** Exercise 7 - Math Object  **********************/
+const decimalNumber = 7.8;
+// Given the above variable, how would you round the value to the nearest whole number (using the Math object)?
+
+// Console log the output to check your work.
+
+
+
+
+
+/********************** Exercise 8 - Math Object  **********************/
+// Dice Game
+// Create a variable that stores the value of a random number between 1 and 6
+
+// Alert the user of the random number.
+
+
+
+
+
+
+/********************** Exercise 9 - NaN  **********************/
+const result1 = 0 / 0; 
+const result2 = "hello" * 5; 
+const result3 = parseInt("abc"); 
+
+// Log the above variables in the console. What is the output?
+
+// Run the following code. How would you edit the following code so that it runs properly?
+const value = 4 / 'four';
+
+// Look up the isNaN() operator in MDN. What does it do? How would you use it?
